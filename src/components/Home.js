e55ce5edf366581cc9497a94b443802979dd7e48
Mehar Singh <meharcharak20@gmail.com>
2024-12-05 10:21:07 +0530

    made some minor changes to the setup the react app.

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Home() {
+  return (
+    <>
+        <Navbar/>
+        <h1 className='container text-center'>This is iNotebook</h1>
+    </>
+  )
+}
+
+export default Home
